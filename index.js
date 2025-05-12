6f7765efddda43ec7b451585ac50b3d73a5926a6
flobatard <fb.batard@gmail.com>
2025-05-12 09:23:06 +0200

    s3 proxy + testing with beta version

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
 const Minio = require('minio')
+const url = require("url")
 
 const app = express();
 const port = 3000;
@@ -10,8 +11,8 @@ const minioClient = new Minio.Client({
     endPoint: 'localhost',
     port: 3900,
     useSSL: false,
-    accessKey: "GKe383543d5326dacc15797540",
-    secretKey: "0c8c3ecb536dc7b2d22894da8c42225f3cf8c8f6551c05929340f97e6ee1dd4f",
+    accessKey: "GK54876079934819b936700961",
+    secretKey: "a662ae756e92fc96855d04bdbbf9c05dd60938e54ab0a016c3c737dbed06dd5b",
     region: 'gtm_test_garage'
 })
 
@@ -19,25 +20,24 @@ const minioClient = new Minio.Client({
 const upload = multer({ dest: 'uploads/' });
 
 app.get('/buckets', async (req, res) => {
-    try{
+    try {
         const buckets = await minioClient.listBuckets()
         res.status(200).json(buckets)
     }
-    catch(err)
-    {
+    catch (err) {
         console.error(err)
         res.status(500).json({})
     }
 })
 
-app.post('/new_bucket', async (req, res) => {
-    try{
-        await minioClient.makeBucket('test-2', 'gtm_test_garage')
+app.post('/new_bucket/:bucket', async (req, res) => {
+    try {
+        const bucket = req.params.bucket
+        await minioClient.makeBucket(bucket, 'gtm_test_garage')
         console.log('Bucket created successfully in garage.')
-        res.status(200).json({message: "Done"})
+        res.status(200).json({ message: "Done" })
     }
-    catch(err)
-    {
+    catch (err) {
         console.error(err)
         res.status(500).json({})
     }
@@ -45,50 +45,51 @@ app.post('/new_bucket', async (req, res) => {
 
 // Route d’upload
 app.post('/upload/:bucket', upload.single('image'), async (req, res) => {
-  const file = req.file;
-
-  const bucket = req.params.bucket
-
-  if (!file) {
-    return res.status(400).send('Aucun fichier envoyé');
-  }
-
-  const fileContent = fs.readFileSync(file.path);
-  
-  const params = {
-    Bucket: bucket,
-    Key: `images/${file.originalname}`,
-    Body: fileContent,
-    ContentType: file.mimetype,
-    ACL: 'public-read'
-  };
-
-  try {
-    minioClient.listObjects('test')
+    const file = req.file;
+    const bucket = req.params.bucket
     
-    minioClient.fPutObject(bucket, `${file.originalname}_2`, file.path, (err, objInfo) => {
-        fs.unlinkSync(file.path); // Nettoyer fichier temporaire
-        if (err) {
-            console.error(err)
-            res.status(500).send("Erreur d'upload")
-        }
-        console.log(objInfo)
-        res.status(200).json(objInfo)
-        return
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Erreur d’upload');
-  }
+
+    if (!file) {
+        return res.status(400).send('Aucun fichier envoyé');
+    }
+
+    const fileContent = fs.readFileSync(file.path);
+
+    const params = {
+        Bucket: bucket,
+        Key: `images/${file.originalname}`,
+        Body: fileContent,
+        ContentType: file.mimetype,
+        ACL: 'public-read'
+    };
+
+    try {
+
+        minioClient.fPutObject(bucket, `images/image/toto.png`, file.path, (err, objInfo) => {
+            fs.unlinkSync(file.path); // Nettoyer fichier temporaire
+            if (err) {
+                console.error(err)
+                res.status(500).send("Erreur d'upload")
+            }
+            console.log(objInfo)
+            res.status(200).json(objInfo)
+            return
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erreur d’upload');
+    }
 });
 
-// Route d’upload
 app.get('/list_bucket/:bucket', async (req, res) => {
     const bucket = req.params.bucket
-  
+
+    const parts = url.parse(req.url, true)
+    const prefix = parts.query['prefix']
+
     try {
         const data = []
-        const stream = minioClient.listObjects(bucket)
+        const stream = minioClient.listObjects(bucket, prefix)
         stream.on('data', function (obj) {
             data.push(obj)
         })
@@ -100,14 +101,51 @@ app.get('/list_bucket/:bucket', async (req, res) => {
             console.log(err)
             res.status(500).json({})
         })
-      
-      
+
+
     } catch (err) {
-      console.error(err);
-      res.status(500).send('Erreur d’upload');
+        console.error(err);
+        res.status(500).send('Erreur d’upload');
     }
-  });
+});
+
+app.get('/presigned/:bucket/*path', async (req, res) => {
+    const bucket = req.params.bucket
+    const name = req.params.path.join('/')
+    
+
+    try {
+        const data = []
+        const url = await minioClient.presignedGetObject(bucket, name)
+        res.status(200).json({url: url})
+
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erreur d’upload');
+    }
+});
+
+app.get('/s3/:bucket/*path', async (req, res) => {
+    try {
+        const bucket = req.params.bucket
+        const name = req.params.path.join('/')
+        const stream = await minioClient.getObject(bucket, name);
+        stream.pipe(res)
+
+    } catch (err) {
+        console.error(err);
+        if (err.code === "NoSuchBucket") return res.status(404).send(err.message)
+        if (err.code === "NoSuchKey") return res.status(404).send(`${err.message} : ${err.resource}`)
+        res.status(500).send("Unexpeced error")
+    }
+})
+
+app.get('/s3_test/*path', (req, res) => {
+    
+    res.status(200).json()
+})
 
 app.listen(port, () => {
-  console.log(`Serveur API écoute sur http://localhost:${port}`);
+    console.log(`Serveur API écoute sur http://localhost:${port}`);
 });
